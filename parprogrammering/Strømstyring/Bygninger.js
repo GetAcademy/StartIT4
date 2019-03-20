a8b1e98285999a42ad288d3c04f4ed92ea62d24a
Orphanum <Orphanum@users.noreply.github.com>
2019-03-20 10:44:32 +0100

    update

---
+++ parprogrammering/Strømstyring/Bygninger.js
@@ -0,0 +1,35 @@
+const BergeskogenBarnehage = {
+    Adresse: "Brekkegata 31",
+    PostNr: "3260",
+    Sted: "Larvik",
+    Kategori: "Barnehage",
+    Areal: 724,
+    Oppvarming: "El"
+};
+
+const ValbyGrendehus = {
+    Adresse: "Søvstråveien 44",
+    PostNr: "3261",
+    Sted: "Larvik",
+    Kategori: "Barnehage",
+    Areal: 373,
+    Oppvarming: "El"
+};
+    
+const SolstadBarnehage = {
+    Adresse: "Agnesveien 14",
+    PostNr: "3292",
+    Sted: "Stavern",
+    Kategori: "Barnehage",
+    Areal: 885,
+    Oppvarming: "El, Varmepumpe"
+};
+
+const Rødbøl = {
+    Adresse: "Ganke Ravei 312",
+    PostNr: "3267",
+    Sted: "Larvik",
+    Kategori: "Barnehage",
+    Areal: 777,
+    Oppvarming: "El, Bio"
+};
