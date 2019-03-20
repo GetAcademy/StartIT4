71459255f4c19c4b2988f16db0e777385141ae43
AnikanSky <willy@getacademy.no>
2019-03-20 09:20:55 +0100

    Create siIfra java.js

---
+++ case/A-Mob/siIfra java.js
@@ -0,0 +1,16 @@
+function siIfra() {
+    document.getElementById('siIfraOmMobbing').innerHTML =
+        `
+            <table>
+                <tr>
+                    <th>hei paa deg</th>
+
+
+
+
+
+                    <th>da praaver vi</th>
+                </tr>
+            </table>
+        `;
+}
