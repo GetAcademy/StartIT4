05295dc1c63134ea28714a34639f740ab080ccdc
Orphanum <Orphanum@users.noreply.github.com>
2019-03-21 12:52:30 +0100

    Update
    Trykk på buttons viser nå all informasjon fra array om bygningene.

--- parprogrammering/Strømstyring/Bygninger.js
+++ parprogrammering/Strømstyring/Bygninger.js
@@ -81,8 +81,7 @@ function show() {
     for (let i = 0; i < data.buildings.length; i++) {
         let building = data.buildings[i];
         html += `<li>
-            <button onclick="selectBuilding(${i})">${building.name}</button> <small>${building.Adresse}</small><br/>
-            <b>${building.Kategori}</b></li><b>${building.Sted}</b>
+            <button onclick="selectBuilding(${i})">${building.name}</button> </b>
         `;
     }
     document.getElementById('buildings').innerHTML = html;
@@ -91,42 +90,13 @@ function show() {
 
 function selectBuilding(index) {
     let building = data.buildings[index];
-    document.getElementById('buildings').innerHTML = `Du valgte ${building.name} `;
+    document.getElementById('buildings').innerHTML = 
+    `<b>
+    Navn: ${building.name}</b><br/>
+    Adresse: ${building.Adresse}</br>
+    PostNr: ${building.PostNr}<br/>
+    Sted: ${building.Sted}<br/>
+    Kategori: ${building.Kategori}<br/>
+    Areal: ${building.Areal}<br/>
+    `;
 }
-
-
-const BergeskogenBarnehage = {
-    Adresse: "Brekkegata 31",
-    PostNr: "3260",
-    Sted: "Larvik",
-    Kategori: "Barnehage",
-    Areal: 724,
-    Oppvarming: "El"
-};
-
-const ValbyGrendehus = {
-    Adresse: "Søvstråveien 44",
-    PostNr: "3261",
-    Sted: "Larvik",
-    Kategori: "Barnehage",
-    Areal: 373,
-    Oppvarming: "El"
-};
-
-const SolstadBarnehage = {
-    Adresse: "Agnesveien 14",
-    PostNr: "3292",
-    Sted: "Stavern",
-    Kategori: "Barnehage",
-    Areal: 885,
-    Oppvarming: "El, Varmepumpe"
-};
-
-const Rødbøl = {
-    Adresse: "Ganke Ravei 312",
-    PostNr: "3267",
-    Sted: "Larvik",
-    Kategori: "Barnehage",
-    Areal: 777,
-    Oppvarming: "El, Bio"
-};
