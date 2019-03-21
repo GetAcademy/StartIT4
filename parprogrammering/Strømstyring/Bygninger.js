a65aceed10d734b5bcb84b62b78ebe2985fd1891
Orphanum <Orphanum@users.noreply.github.com>
2019-03-21 11:05:25 +0100

    Update
    Lagt til en array som holder bygningene

--- parprogrammering/Strømstyring/Bygninger.js
+++ parprogrammering/Strømstyring/Bygninger.js
@@ -1,7 +1,7 @@
 var data = {
     buildings: [
         {
-            name: 'BergeskogenBarnehage',
+            name: 'Bergeskoge Barnehage',
             Adresse: "Brekkegata 31",
             PostNr: "3260",
             Sted: "Larvik",
@@ -19,7 +19,7 @@ var data = {
             ]
         },
         {
-            name: 'ValbyGrendehus',
+            name: 'Valby Grendehus',
             Adresse: "Søvstråveien 44",
             PostNr: "3261",
             Sted: "Larvik",
@@ -35,8 +35,44 @@ var data = {
                     energy: 4.3
                 },
             ]
-        }
-    ]
+        },
+        {
+            name: "Solstad Barnehage",
+            Adresse: "Agnesveien 14",
+            PostNr: "3292",
+            Sted: "Stavern",
+            Kategori: "Barnehage",
+            Areal: 885,
+            Oppvarming: "El, Varmepumpe",
+            powerConsumptionHistory: [
+                {
+                    date: '2019-03-02 14:00',
+                    energy: 5.3
+                }, {
+                    date: '2019-03-02 13:00',
+                    energy: 4.3
+                }, 
+            ]
+        },
+        {
+            name: "Rødbøl",
+            Adresse: "Ganke Ravei 312",
+            PostNr: "3267",
+            Sted: "Larvik",
+            Kategori: "Barnehage",
+            Areal: 777,
+            Oppvarming: "El, Bio",
+            powerConsumptionHistory: [
+                {
+                    date: '2019-03-02 15.00',
+                    energy: 8.3
+                }, {
+                    date: '2019-03-02 16.00',
+                    energy: 3.8
+                },
+            ]
+        },    
+    ]   
 };
 
 show();
@@ -46,13 +82,10 @@ function show() {
         let building = data.buildings[i];
         html += `<li>
             <button onclick="selectBuilding(${i})">${building.name}</button> <small>${building.Adresse}</small><br/>
-            <b>${building.Kategori}</b></li>
+            <b>${building.Kategori}</b></li><b>${building.Sted}</b>
         `;
     }
     document.getElementById('buildings').innerHTML = html;
-
-    // var x = 'Hei ' + navn + '!';
-    // var y = `Hei ${navn}!`;
 
 }
 
