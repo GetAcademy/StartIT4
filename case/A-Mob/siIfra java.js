941b693559e672b238ac4ac286aa2a24b9471062
AnikanSky <willy@getacademy.no>
2019-03-21 13:10:58 +0100

    Update siIfra java.js

--- case/A-Mob/siIfra java.js
+++ case/A-Mob/siIfra java.js
@@ -1,19 +1,3 @@
-function siIfra() {
-    document.getElementById('siIfraOmMobbing').innerHTML =
-        `
-            <table>
-                <tr>
-                    <th>hei paa deg</th>
-
-
-
-
-
-                    <th>da praaver vi</th>
-                </tr>
-            </table>
-        `;
-}
 function visMeny() {
     document.getElementById('innhold').innerHTML = 
     `
