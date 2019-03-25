9b63dd8cca32803d3ec6a0f0fae8eefab54bc003
JaranJB <47241821+JaranJB@users.noreply.github.com>
2019-03-25 13:25:43 +0100

    update case

--- student/jaran/Mobil Case/final screen.js
+++
@@ -1,8 +0,0 @@
-﻿function finaleScreen() {
-    var mål1 = document.getElementById('mål1').value;
-    content.innerHTML = `
-                <div class="overskrift back" onclick="newGoal()">←</div> 
-                <h2 class="overskrift">Finale</h2>
-                <div>${mål1}</div>
-        `
-}
