c50975c1055638dae42e9684026d7698a8bf6033
AnikanSky <willy@getacademy.no>
2019-03-26 11:39:25 +0100

    Delete A-mob visOgSkjulMeny.js

--- case/A-Mob/A-mob visOgSkjulMeny.js
+++
@@ -1,7 +0,0 @@
-function visOgSkjulMeny() {
-    let meny = document.getElementById('meny');
-    let displayNow = meny.style.display;
-    meny.style.display = displayNow == 'none' ? 'block' : 'none';
-
-    //document.getElementById('page').classList.toggle('pageUtenMeny');
-}
