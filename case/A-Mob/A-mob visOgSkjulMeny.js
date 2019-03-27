922ccb36c6d5a91ce85e1fa29d3c1c1824756547
AnikanSky <willy@getacademy.no>
2019-03-27 14:41:11 +0100

    Update A-mob visOgSkjulMeny.js

--- case/A-Mob/A-mob visOgSkjulMeny.js
+++ case/A-Mob/A-mob visOgSkjulMeny.js
@@ -9,6 +9,7 @@
 //     //document.getElementById('page').classList.toggle('pageUtenMeny');
     function nåErMenyBorte() {
         let meny = document.getElementById('meny');
+        let displaynow = meny.style.display;
         meny.style.display = displayNow == 'none';
     }
 
