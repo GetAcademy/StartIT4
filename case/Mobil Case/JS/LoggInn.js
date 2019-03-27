c8e5c678d062b098c50c5827d99e67ef08885ff9
EMILO <emilberolsen@gmail.com>
2019-03-27 11:58:58 +0100

    Har ikke lagt til noe ekstra CSS enda.

---
+++ case/Mobil Case/JS/LoggInn.js
@@ -0,0 +1,8 @@
+function LoggInn() {
+    document.getElementById('content').innerHTML = `<input type="text" placeholder="USERNAME" />
+    <input type="password" placeholder="PASSWORD" />
+    <div>
+        <button>LOGG INN</button>
+        <button>LAG NY BRUKER</button>
+    </div>`;
+}
