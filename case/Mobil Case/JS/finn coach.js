09f1f5100d316a7a212441a9b61dbca199c1d079
JaranJB <47241821+JaranJB@users.noreply.github.com>
2019-03-28 12:26:19 +0100

    Oppdatert css

--- case/Mobil Case/JS/finn coach.js
+++ case/Mobil Case/JS/finn coach.js
@@ -1,6 +1,9 @@
 ﻿function coachScreen() {
-    content.innerHTML = `
-                <div class="overskrift back" onclick="nyEvaluering()">←</div> 
-                <h2 class="overskrift">Coach</h2>
-        `
+    header.innerHTML = `<div class="overskrift back" onclick="nyEvaluering()">←</div> 
+        <h2 class="overskrift">Coach</h2 >`;
+    content.classList.length = 0;
+    content.classList.add('myGrid');
+    content.classList.add('mainEvaluatePage');
+    content.innerHTML = ''
+        
 }
