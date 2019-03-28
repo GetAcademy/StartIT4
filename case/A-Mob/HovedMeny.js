1f7cf7f6dd607b50652f843343fa7d5beb7dc7ae
AnikanSky <willy@getacademy.no>
2019-03-28 15:30:55 +0100

    Update HovedMeny.js

--- case/A-Mob/HovedMeny.js
+++ case/A-Mob/HovedMeny.js
@@ -7,19 +7,24 @@ function visMeny() {
         </tr>
         <tr>
             <div>
-                <button class="fixed1" onclick="visSiIfraOmMobbing()">Si ifra om mobbing</button>
+                <tr>
+                    <button class="fixed1" onclick="visSiIfraOmMobbing()">Si ifra om mobbing</button>
+                </tr>
+                <tr>
+                    <button class="fixed1" onclick="visHvaErMobbing()">Hva er mobbing</button>
+                </tr>
+                <tr>
+                    <button class="fixed1" onclick="visHvordanTaVarePåAndre();nåErMenyBorte();">Hvordan ta vare på andre</button>
+                </tr>
+                <tr>
+                    <button class="fixed1" onclick="visSelvevaluering()">Selvevaluering</button>
+                </tr>
             </div>
-        <tr>
-            <div><button class="fixed1" onclick="visHvaErMobbing()">Hva er mobbing</button></div>
-        </tr>
-        <tr>
-            <div><button class="fixed1" onclick="visHvordanTaVarePåAndre();nåErMenyBorte();">Hvordan ta vare på andre</button></div>
-        </tr>
-        <tr>
-            <div><button class="fixed1" onclick="visSelvevaluering()">Selvevaluering</button></div>
-        </tr>
-        <tr>
-            <div><button class="fixed2">Logg ut</div></button>
+            <div>
+                <tr>
+                    <button class="fixed2">Logg ut</button>
+                </tr>
+            </div>
         </tr>
     </tbody>
     </table>`
