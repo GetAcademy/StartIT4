a316aa966d43ae354bd408dd0d39e4648f44ce18
larsmn <47142673+larsmn@users.noreply.github.com>
2019-03-28 09:30:59 +0100

    HvaErMobbing litt info

--- case/A-Mob/HvaErMobbing.js
+++ case/A-Mob/HvaErMobbing.js
@@ -6,7 +6,14 @@ function visHvaErMobbing() {
         <tbody>
             <tr>
                 <h1 onclick="visHvaErMobbing()">Hva er Mobbing?</h1>
+            </tr>
             <tr>
+                <h5> Kjennetegn p&#229; mobbing. </h5>
+            </tr>
+            <tr>
+                <li>Negativ eller ondsinnet atferd mot andre.</li>
+                <li>N&#229;r noe negativt foreg&#229;r over lengre tid.</li>
+            </tr>
         </tbody>
     </table>
     `;
