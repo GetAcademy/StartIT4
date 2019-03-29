34d5db3a4d8a37f970519601148a929e5b7cec68
Robart Andersen <47173246+Robart92@users.noreply.github.com>
2019-03-29 14:52:47 +0100

    sjallabajs

--- case/A-Mob/HovedMeny.js
+++ case/A-Mob/HovedMeny.js
@@ -1,29 +1,23 @@
 function visMeny() {
     document.getElementById('innhold').innerHTML =
-        `<table>
-    <tbody>
-        <tr>
-            <h1 class="to">Meny</h1>
-        </tr>
-        <tr>
+        `<h1 class="to">Meny</h1>
+       
             <div>
-                <tr>
+               
                     <button class="fixed1" onclick="visSiIfraOmMobbing()">Si ifra om mobbing</button>
-                </tr>
-                <tr>
+               
+             
                     <button class="fixed1" onclick="visHvaErMobbing()">Hva er mobbing</button>
-                </tr>
-                <tr>
+               
                     <button class="fixed1" onclick="visHvordanTaVarePåAndre();nåErMenyBorte();">Hvordan ta vare på andre</button>
-                </tr>
-                <tr>
+               
                     <button class="fixed1" onclick="visSelvevaluering()">Selvevaluering</button>
-                </tr>
+                
             </div>
             <div>
-                <tr>
-                    <button class="fixed2">Logg ut</button>
-                </tr>
+               
+                    <button class="fixed1 fixed2">Logg ut</button>
+               
             </div>
         </tr>
     </tbody>
