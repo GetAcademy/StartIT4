ed09cc22c8ae9492a2746d96a13cca4463e761cd
AnikanSky <willy@getacademy.no>
2019-04-01 14:45:14 +0200

    selvelaluering

--- case/A-Mob/Selvevaluering.js
+++ case/A-Mob/Selvevaluering.js
@@ -8,22 +8,22 @@ document.getElementById('innhold').innerHTML =
                 <tr>
                     <div>åssen har dagen vært?</div>
                 </tr>
-                <div>
-                    <div id="a" onclick="doClick(this)" style="background-color: pink">A</div>
-                    <div id="b" onclick="doClick(this)" style="background-color: lightgreen">B</div>       
-                </div>
                 <div id="exodia">
-                    <div onclick="">&#128516;</div>
-                    <div onclick="">&#128528;</div>
-                    <div onclick="">&#128543;</div></br>
-                    bra. ok. dårlig
-                 </div>
-                <div id="info" onclick="doClick(this)">Tykk på en firkant!</div>
-                <input type="text">
-                <div>
-                    <button onclick="visSelvevaluering()">Neste</button>
-                    <button onlick="visMeny()">Lever</button> 
+                    <div id="quiz">
+                        <label>
+                            <input type="radio" name="question0" value="a">&#128516
+                        </label>
+                        <label>
+                            <input type="radio" name="question0" value="a">&#128528
+                        </label>
+                        <label>
+                            <input type="radio" name="question0" value="a">&#128543
+                        </label>
+                    </div>
+                    <button id="submit">Get Results</button>
+                    <div id="results"></div>
                 </div>
+                <input type="text">
             </table>
         `;
 }
