c23e0a2c364b7f665186399afbf8c1de50bc0811
JaranJB <47241821+JaranJB@users.noreply.github.com>
2019-04-01 12:56:31 +0200

    fix

--- case/Mobil Case/JS/add text.js
+++ case/Mobil Case/JS/add text.js
@@ -1,4 +1,4 @@
 function addtxt() {
     var textDiv = document.getElementById('textDiv');
-    textDiv.innerHTML += '<textarea class="mainButton"></textarea><br />';
+    textDiv.innerHTML += '<textarea class="mainButton"></textarea><button></button><br />';
 }
