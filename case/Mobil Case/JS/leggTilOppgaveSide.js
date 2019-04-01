1f9d937682cf3ad9a569dc15032d72c5ed6d1aa7
tryta <47241882+tryta@users.noreply.github.com>
2019-04-01 11:00:02 +0200

    Add files via upload

---
+++ case/Mobil Case/JS/leggTilOppgaveSide.js
@@ -0,0 +1,13 @@
+﻿function leggTilOppgaveSide() {
+    header.innerHTML = '<h1>Legg til oppgave</h1>';
+    content.classList.length = 0;
+    content.classList.add('myGrid');
+    content.classList.add('leggTilOppgaveSide');
+    content.innerHTML = `
+    <div style="align-self: center">
+        Skriv hva oppgaven skal være i tekstfeltet under
+    </div>
+    <div><textarea class="bigText2"></textarea></div>
+    <div><button onclick="showMyGoalsScreen()">Tilbake</button</div>
+    `;
+}
---
+++ case/Mobil Case/JS/main.js
@@ -0,0 +1,16 @@
+﻿function showMainScreen() {
+    header.innerHTML = '<h1>Hovedside</h1>';
+    content.setAttribute('class', 'mainPageContentGrid');
+    content.innerHTML = `
+    <button class="mainGoals" onclick="showMyGoalsScreen()">
+      Hovedmål: 1</br>
+Hovedmål: 2</br>
+Hovedmål: 3
+    </button>
+      <div><button class="mainButton" onclick="ukentligEvaluering()">Ukentlig evaluering</button></div>
+      <div><button class="mainButton" onclick="showEvaluateScreen()">Daglig evaluering</button></div>
+      <div><button class="mainButton" onclick="lagNyttMål()">Nytt mål</button></div>
+      <div><button class="mainButton" onclick="lagDagbok()">Dagbok</button></div>
+      <div><button class="mainButton" onclick="kalender()">Kalender</button></div>
+    `;
+}
