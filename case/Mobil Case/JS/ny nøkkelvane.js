2ff688b8a1785104bd34fefd9e098d714aac210a
JaranJB <47241821+JaranJB@users.noreply.github.com>
2019-04-01 13:00:09 +0200

    scroll

--- case/Mobil Case/JS/ny nøkkelvane.js
+++ case/Mobil Case/JS/ny nøkkelvane.js
@@ -7,7 +7,7 @@
     content.classList.add('mainEvaluatePage');
     content.innerHTML = `
                 <p class="mainButton">Hvilke vaner trenger du for å nå målet?</p>
-                <div id="textDiv"> <textarea class="mainButton"></textarea><br /></div>
+                <div class="Textbox" id="textDiv"> <textarea class="mainButton"></textarea><br /></div>
                 <div id="nyVane" onclick="addtxt()" style="font-size:50px; text-align:center;">⁺</div><br />
                 <br /><button onclick="nyAktivitet()" class="mainButton">Neste</button>
                             `}
