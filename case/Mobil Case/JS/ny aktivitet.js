084320ca3ae289d1b578e69a9d2c12e954e3e8e4
JaranJB <47241821+JaranJB@users.noreply.github.com>
2019-04-03 14:15:13 +0200

    Update ny aktivitet.js

--- case/Mobil Case/JS/ny aktivitet.js
+++ case/Mobil Case/JS/ny aktivitet.js
@@ -1,13 +1,9 @@
 ﻿function nyAktivitet() {
-    header.innerHTML = `<div class="overskrift back" onclick="nyNøkkelvane()">←</div>
-        <h2 class="overskrift">Aktiviteter</h2>
-        <div class="info" onclick="infoScreen()">🛈</div>`;
-    content.classList.length = 0;
-    content.classList.add('myGrid');
-    content.classList.add('mainEvaluatePage');
+    
     content.innerHTML = `
-                <p class="mainButton">Hvilke aktiviteter vil være viktige for deg hvis du skal nå dette målet?</p>
-                <div id="textDiv"> <textarea class="mainButton"></textarea><br /></div>
+                <div class="overskrift back" onclick="nyNøkkelvane()">←</div>
+                <p class="button">Hvilke aktiviteter vil være viktige for deg hvis du skal nå dette målet?</p>
+                <div id="textDiv"> <textarea class="button" style="background-color:white;color:black"></textarea><br /></div>
                 <div id="nyVane" onclick="addtxt()" style="font-size:50px; text-align:center;">⁺</div><br />
                 <br /><button onclick="nyEvaluering()" class="button">Neste</button>
                         `}
