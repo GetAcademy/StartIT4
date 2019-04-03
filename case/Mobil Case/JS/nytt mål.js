b4661315e76c4294d274a81ca20dd680a6f102b7
JaranJB <47241821+JaranJB@users.noreply.github.com>
2019-04-03 14:08:09 +0200

    Update nytt mål.js

--- case/Mobil Case/JS/nytt mål.js
+++ case/Mobil Case/JS/nytt mål.js
@@ -1,11 +1,11 @@
 ﻿function newGoal() {
    
     mainextra.innerHTML = `
-                <p class="mainButton">Hva vil du oppnå?</p>
-                <textarea class="mainButton"></textarea>
-                <p class="mainButton">Forkort det til en setning.</p>
-                <textarea class="mainButton"></textarea>
-                <p class="mainButton">Hvorfor vil du nå dette målet?</p>
-                <textarea class="mainButton"></textarea class="mainButton"> <br />
+                <p class="button">Hva vil du oppnå?</p>
+                <textarea class="button" style="background-color:white"></textarea>
+                <p class="button">Forkort det til en setning.</p>
+                <textarea class="button" style="background-color:white"></textarea>
+                <p class="button">Hvorfor vil du nå dette målet?</p>
+                <textarea class="button" style="background-color:white"></textarea> <br />
                 <br /><button onclick="nyNøkkelvane()" class="button">Neste</button>
                                   `}
