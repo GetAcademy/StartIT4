94716fac0c9e17640f281e68d32a45a1d1afb8db
JaranJB <47241821+JaranJB@users.noreply.github.com>
2019-04-03 14:39:42 +0200

    Update daglig evaluering.js

--- case/Mobil Case/JS/daglig evaluering.js
+++ case/Mobil Case/JS/daglig evaluering.js
@@ -2,7 +2,7 @@
     
     mainextra.innerHTML = `
                 <div class="overskrift back" onclick="nyEvaluering()">←</div>
-                <p class="mainButton">Velg</p>
+                <p class="button">Velg</p>
                 <textarea class="button" style="background-color:white;color:black">Humør</textarea><br />
                 <textarea class="button" style="background-color:white;color:black">Mentalt Tilstede</textarea><br />
                 <textarea class="button" style="background-color:white;color:black">Kvalitet på arbeid</textarea><br />
