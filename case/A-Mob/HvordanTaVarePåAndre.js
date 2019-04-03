7c42b0219c3354f5ee87fc03ec949d996dc014aa
larsmn <47142673+larsmn@users.noreply.github.com>
2019-04-03 14:31:03 +0200

    Update HvordanTaVarePåAndre.js

--- case/A-Mob/HvordanTaVarePåAndre.js
+++ case/A-Mob/HvordanTaVarePåAndre.js
@@ -4,14 +4,14 @@ function visHvordanTaVarePåAndre() {
     <table>
         <tbody>
             <tr>
-                <u><h1 onclick="()">Hvordan ta vare på andre?</h1></u>
+                <u class="underline"><h1 class="hvaermobbing" onclick="()">Hvordan ta vare på andre?</h1></u>
             </tr>
             <tr>
-                <li> Gjør mot andre slik du vil de skal gjøre mot deg. </li>
-                <li> Inviter nye personer til å bli med på ting. </li>
-                <li> Tenk på andre sine følelser før du sier noe slemt. </li>
-                <li> Si "Hei" når du møter noen. </li>
-                <li> Vær snill med hverandre. </li><br>
+                <li class="fiksme"> Gjør mot andre slik du vil de skal gjøre mot deg. </li>
+                <li class="fiksme"> Inviter nye personer til å bli med på ting. </li>
+                <li class="fiksme"> Tenk på andre sine følelser før du sier noe slemt. </li>
+                <li class="fiksme"> Si "Hei" når du møter noen. </li>
+                <li class="fiksme"> Vær snill med hverandre. </li><br>
                 <div class="iframe"><iframe width="560" height="315" src="https://www.youtube.com/embed/BpDwzOvvgsE" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></div>
             </tr>
         <tbody>
