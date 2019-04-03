cb59a9baca963563d019d64aaa09f48533b1286e
JaranJB <47241821+JaranJB@users.noreply.github.com>
2019-04-03 13:50:46 +0200

    Update ny nøkkelvane.js

--- case/Mobil Case/JS/ny nøkkelvane.js
+++ case/Mobil Case/JS/ny nøkkelvane.js
@@ -3,7 +3,7 @@
     mainextra.innerHTML = `
                 <div class="overskrift back" onclick="newGoal()">←</div>
                 <p class="mainButton">Hvilke vaner trenger du for å nå målet?</p>
-                <div class="Textbox" id="textDiv"> <textarea class="mainButton"></textarea><br /></div>
+                <div class="Textbox" id="textDiv"> <textarea class="button"></textarea><br /></div>
                 <div id="nyVane" onclick="addtxt()" style="font-size:50px; text-align:center;">⁺</div><br />
                 <br /><button onclick="nyAktivitet()" class="button">Neste</button>
                             `}
