61539562cd3e0f5ea478623a67dbc708e1702e4d
JaranJB <47241821+JaranJB@users.noreply.github.com>
2019-04-04 13:27:40 +0200

    Update lagDagbok.js

--- case/Mobil Case/JS/lagDagbok.js
+++ case/Mobil Case/JS/lagDagbok.js
@@ -4,7 +4,7 @@ function lagDagbok() {
     <div style="align-self: center">
         Skriv litt om dagen i dag.
     </div>
-    <div><textarea class="bigText2"></textarea></div>
-    <div><button onclick="showMainScreen()">Lagre og ferdig</button></div>
+    <div><textarea class="button" style="background-color:white;color:black"></textarea></div>
+    <div><button onclick="showMainScreen()" class="button">Lagre og ferdig</button></div>
     `;
 }
