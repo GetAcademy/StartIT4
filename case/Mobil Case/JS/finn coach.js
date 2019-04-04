2b5633f590fe142d58a4319757736a5c26f056de
JaranJB <47241821+JaranJB@users.noreply.github.com>
2019-04-04 12:19:30 +0200

    Update finn coach.js

--- case/Mobil Case/JS/finn coach.js
+++ case/Mobil Case/JS/finn coach.js
@@ -1,9 +1,5 @@
 ﻿function coachScreen() {
-    header.innerHTML = `<div class="overskrift back" onclick="nyEvaluering()">←</div> 
-        <h2 class="overskrift">Coach</h2 >`;
-    content.classList.length = 0;
-    content.classList.add('myGrid');
-    content.classList.add('mainEvaluatePage');
-    content.innerHTML = ''
+    
+   menuextra.innerHTML = ''
         
 }
