20c21ccdac3e8f07eef3e2a322b7cf98cfb380b9
JaranJB <47241821+JaranJB@users.noreply.github.com>
2019-04-04 12:23:01 +0200

    Update ukentligEvalueringSideTO.js

--- case/Mobil Case/JS/ukentligEvalueringSideTO.js
+++ case/Mobil Case/JS/ukentligEvalueringSideTO.js
@@ -1,9 +1,6 @@
 ﻿function ukentligEvalueringSideTO() {
-    header.innerHTML = '<h1>Ukentlig evaluering</h1>';
-    content.classList.length = 0;
-    content.classList.add('myGrid');
-    content.classList.add('ukentligEvalueringSideTO');
-    content.innerHTML = `
+    
+    menuextra.innerHTML = `
     <div style="align-self: center">
         Hva kan gjøres bedre neste uke?
     </div>
