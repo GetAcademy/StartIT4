225cd9b96128cd0b88e8fcc9076c987452853b22
JaranJB <47241821+JaranJB@users.noreply.github.com>
2019-04-04 13:28:15 +0200

    Update lagDagbok.js

--- case/Mobil Case/JS/lagDagbok.js
+++ case/Mobil Case/JS/lagDagbok.js
@@ -1,7 +1,7 @@
 function lagDagbok() {
     
     mainextra.innerHTML = `
-    <div style="align-self: center">
+    <div class="button">
         Skriv litt om dagen i dag.
     </div>
     <div><textarea class="button" style="background-color:white;color:black"></textarea></div>
