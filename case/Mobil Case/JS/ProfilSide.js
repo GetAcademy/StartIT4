3a82765405c86fcd311e8a0bc9cf1f1347ec9c0a
JaranJB <47241821+JaranJB@users.noreply.github.com>
2019-04-08 10:09:46 +0200

    Update ProfilSide.js

--- case/Mobil Case/JS/ProfilSide.js
+++ case/Mobil Case/JS/ProfilSide.js
@@ -1,8 +1,8 @@
 function ProfileSide() {
     document.getElementById('mainextra').innerHTML = `<img src="http://www.bakkahonline.com/assets/images/user-avatar.png" />
-    <div>COACH</div>
-    <div>BRUKERNAVN</div>
-    <div>INNSTILLINGER</div>
-    <div>MINE MÅL</div>
-    <div>LOGG UT</div>`;
+    <div class="button">COACH</div>
+    <div class="button">BRUKERNAVN</div>
+    <div class="button">INNSTILLINGER</div>
+    <div class="button">MINE MÅL</div>
+    <div class="button">LOGG UT</div>`;
 }
