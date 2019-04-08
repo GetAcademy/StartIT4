8bb481ca3f2351b19f494f276845d81fffd11dbb
JaranJB <47241821+JaranJB@users.noreply.github.com>
2019-04-08 13:46:08 +0200

    Update ny nøkkelvane.js

--- case/Mobil Case/JS/ny nøkkelvane.js
+++ case/Mobil Case/JS/ny nøkkelvane.js
@@ -1,5 +1,10 @@
 ﻿function nyNøkkelvane() {
-    
+    var txt1 = document.getElementById('txt1').value;
+     var txt2 = document.getElementById('txt2').value;
+     var txt3 = document.getElementById('txt3').value;
+     goalOBJ.txt1 = txt1;
+     goalOBJ.txt2 = txt2;
+     goalOBJ.txt3 = txt3;
     mainextra.innerHTML = `
                 <div class="overskrift back" onclick="newGoal()">←</div>
                 <p class="button">Hvilke vaner trenger du for å nå målet?</p>
