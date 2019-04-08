98b89037e0ae82c2df21a0304bc40fd63a4b58ee
JaranJB <47241821+JaranJB@users.noreply.github.com>
2019-04-08 11:08:24 +0200

    Update LoggInn.js

--- case/Mobil Case/JS/LoggInn.js
+++ case/Mobil Case/JS/LoggInn.js
@@ -2,7 +2,7 @@ function LoggInn() {
     document.getElementById('mainextra').innerHTML = `<input type="text" placeholder="USERNAME" /><br/>
     <input type="password" placeholder="PASSWORD" />
     <div>
-        <button onclick="showMainScreen()" class="button">LOGG INN</button>
-        <button onclick="Rolle()" class="button">LAG NY BRUKER</button>
+        <button onclick="Rolle()" class="button">LOGG INN</button>
+        <button onclick="LagNyBruker()" class="button">LAG NY BRUKER</button>
     </div>`;
 }
