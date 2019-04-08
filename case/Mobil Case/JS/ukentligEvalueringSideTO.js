c61f4df9ca86f8d5c6531bb2e390c4f04625cf0e
JaranJB <47241821+JaranJB@users.noreply.github.com>
2019-04-08 10:57:25 +0200

    Update ukentligEvalueringSideTO.js

--- case/Mobil Case/JS/ukentligEvalueringSideTO.js
+++ case/Mobil Case/JS/ukentligEvalueringSideTO.js
@@ -6,6 +6,6 @@
     </div>
     <div><textarea class="button" style="background-color:white;color:black"></textarea></div>
     <div><button class="button" onclick="ukentligEvalueringSideTRE()">Neste</button></div>
-    <div><button class="button" onclick="ukentligEvalueringSide()">Neste</button><div>
+    <div><button class="button" onclick="ukentligEvaluering()">Tilbake</button><div>
     `;
 }
