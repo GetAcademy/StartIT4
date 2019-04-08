cacdaf1c6b6b1cd64df5d81ee47767fa7ba8c205
JaranJB <47241821+JaranJB@users.noreply.github.com>
2019-04-08 14:04:39 +0200

    Update main.js

--- case/Mobil Case/JS/main.js
+++ case/Mobil Case/JS/main.js
@@ -1,9 +1,7 @@
 ﻿function showMainScreen() {
-     
+     txt2="Lag Mål"
     mainextra.innerHTML = `
-    <button class="button" onclick="showMyGoalsScreen()">
-      ${goalOBJ.txt2}
-    </button>
+    <button class="button" onclick="showMyGoalsScreen()">${goalOBJ.txt2}</button>
       <div><button class="button" onclick="ukentligEvaluering()">Ukentlig evaluering</button></div>
       <div><button class="button" onclick="dagligEvaluering()">Daglig evaluering</button></div>
       <div><button class="button" onclick="newGoal()">Nytt mål</button></div>
