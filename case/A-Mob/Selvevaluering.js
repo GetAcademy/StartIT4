2fd6d5b0a1cb8e95c7de68b94b6cfffa0fe70264
AnikanSky <willy@getacademy.no>
2019-04-09 15:07:13 +0200

    bare css som er igjen nå

--- case/A-Mob/Selvevaluering.js
+++ case/A-Mob/Selvevaluering.js
@@ -3,12 +3,13 @@ var infoDiv = document.getElementById('info');
 var questions = [];
 var answers = [];
 
+
 function visSelvevaluering() {
     document.getElementById('innhold').innerHTML =
         `
                 <table>
                     <tr>
-                        <h1>Evaluering</h1>
+                        <h1 class="Evaluering">Evaluering</h1>
                     </tr>
                     <tr>
                         <input id="godJul" type="text" />
@@ -27,29 +28,37 @@ function showNextQuestion() {
     if (questions.length > 0 && question == null) {
         let html = '';
         for (let answer of answers) {
-            html += `${answer.questionText} <b>${answer.howAreYouText} <b>${answer.answerText}</b><br/>`;
+            html += `${answer.questionText} <b>${answer.howAreYouText} <b><br/>${answer.answerText}<br/>`;
         }
         questionDiv.innerHTML = html;
     } else {
         console.log(questionDiv);
         questionDiv.innerHTML = `
- 
             <h3>${question || ''}</h3>
             <div id="mainForm" name="mainForm">
-                <input class="happyFace" onclick="enableButton();" type="radio" name="howAreYou" value="&#128516" /><span class="happyChecked">&#128516</span>
-                <input class="happyFace" onclick="enableButton();" type="radio" name="howAreYou" value="&#128528" /><span class="happyChecked">&#128528</span>
-                <input class="happyFace" onclick="enableButton();" type="radio" name="howAreYou" value="&#128543" /><span class="happyChecked">&#128543</br>
+                <div id="happy" class="faces" onclick="enableButton('&#128516', this);" type="radio">&#128516</div>
+                <div id="netrual" class="faces" onclick="enableButton('&#128528', this);" type="radio">&#128528</div>
+                <div id="sad" class="faces" onclick="enableButton('&#128543', this);" type="radio">&#128543</div></br>
             </div>
-            <input id="answer" type="text"/>
-            <button id="sendIn" disabled="true" onclick="answer('${question}')">Svar</button>
-
+            <input class="textBox" id="answer" type="textbox"/></br>
+            <button class="evalueringSendInn" id="sendIn" disabled="true" onclick="answer('${question}')">Send inn</button>
      `;
     }
 
 }
-function enableButton() {
+var happyOrSad;
+var buttonClicked;
+
+function enableButton(happyOrSadValue, smiley) {
+    happyOrSad = happyOrSadValue;
+    for (let i = 0; i < 3; i++) {
+        document.getElementsByClassName("faces")[i].style.backgroundColor = "#EC8B5E";
+        
+    }
+    smiley.style.backgroundColor = "green";
     var enable = document.getElementById("sendIn");
     enable.disabled = false;
+    
 }
 
 function getNextQuestion() {
@@ -81,14 +90,14 @@ function heisan() {
 function answer(question) {
     let answerInput = document.getElementById('answer');
     let answer = answerInput.value;
-    let button = Array.from(document.getElementById('mainForm').children);
-    let howAreYou = button.find(function (element) {
-        return element.checked;
-    }).value;
-    console.log(howAreYou)
+    //let button = Array.from(document.getElementById('mainForm').children);
+    // let howAreYou = button.find(function (element) {
+    //     return element.checked;
+    // }).value;
+    console.log(happyOrSad);
     let questionAndAnswer = {
         questionText: question,
-        howAreYouText: howAreYou,
+        howAreYouText: happyOrSad,
         answerText: answer
     };
     answers.push(questionAndAnswer);
