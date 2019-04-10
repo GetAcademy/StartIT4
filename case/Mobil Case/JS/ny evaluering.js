00ace68dce0acbb7c00239dd1e1a589e0548363c
JaranJB <47241821+JaranJB@users.noreply.github.com>
2019-04-10 13:56:01 +0200

    lagring

--- case/Mobil Case/JS/ny evaluering.js
+++ case/Mobil Case/JS/ny evaluering.js
@@ -1,9 +1,1 @@
-﻿function nyEvaluering() {
-  
-    mainextra.innerHTML = `
-                <div class="overskrift back" onclick="nyAktivitet()">←</div>
-                <button class="button" onclick="dailyScreen()">Daglig</button><br />
-                <button class="button" onclick="weeklyScreen()">Ukentlig</button><br />
-                <button  class="button" onclick="coachScreen()">Coach</button><br />
-                <br /><button onclick="showMainScreen()" class="button">Fullfør</button>
-            `}
+﻿
