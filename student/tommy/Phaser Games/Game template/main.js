68927f54dd0c362a48b176e514487cd90cfb30fd
TommyWi <47241834+TommyWi@users.noreply.github.com>
2019-04-12 09:22:56 +0200

    creating basic template

---
+++ student/tommy/Phaser Games/Game template/main.js
@@ -0,0 +1,9 @@
+window.onload = function () {
+    var config = {
+        type: Phaser.AUTO,
+        width: 480,
+        height: 640,
+        parent: 'phaser-game',
+        scene: [SceneMain]
+    };
+}
