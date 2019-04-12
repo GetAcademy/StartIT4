046b7f19570bc3ac78f6acda45b59cd97c60dc4d
TommyWi <47241834+TommyWi@users.noreply.github.com>
2019-04-12 14:21:11 +0200

    Practicing sprite movment; tweens and text

--- student/tommy/Phaser Games/ScratchPad/js/SceneMain.js
+++ student/tommy/Phaser Games/ScratchPad/js/SceneMain.js
@@ -3,11 +3,40 @@ class SceneMain extends Phaser.Scene {
         super('SceneMain');
     }
     preload() {
-        this.load.image("face", "images/face.png");
+        this.load.spritesheet('boy', 'images/boy.png', { frameWidth: 120, frameHeight: 200 });
     }
     create() {
-        this.face = this.add.image(100, 200, "face");
-        console.log("Ready!");
+        this.char = this.add.sprite(0, game.config.height / 2, "boy");
+
+        var frameNames = this.anims.generateFrameNumbers('boy');
+
+        this.anims.create({
+            key: 'walk',
+            frames: frameNames,
+            frameRate: 8,
+            repeat: -1
+        });
+        this.char.play('walk');
+        this.doWalk();
+        this.tex1 = this.add.text(200, 300, "HELLO!");
     }
-    update() { }
+
+    doWalk() {
+        this.tweens.add({
+            targets: this.char,
+            duration: 5000,
+            x: game.config.width,
+            onComplete: this.onCompleteHandler.bind(this),
+            
+        });
+    }
+    onCompleteHandler(tween, targets, custom) {
+        var char = targets[0];
+        char.x = 0;
+        this.doWalk();
+    }
+    update() {
+
+    }
+    
 }
