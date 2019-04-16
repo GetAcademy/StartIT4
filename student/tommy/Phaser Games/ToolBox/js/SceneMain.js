e229ad466182b335a1b90be6d91009dda268d894
TommyWi <47241834+TommyWi@users.noreply.github.com>
2019-04-16 13:47:06 +0200

    added textbutton constructor to toolbox

--- student/tommy/Phaser Games/ToolBox/js/SceneMain.js
+++ student/tommy/Phaser Games/ToolBox/js/SceneMain.js
@@ -3,17 +3,19 @@ class SceneMain extends Phaser.Scene {
         super('SceneMain');
     }
     preload() {
+        this.load.image("button1", "images/ui/buttons/2/1.png");
 
     }
     create() {
-
+        var flatbutton = new FlatButton({ scene: this, key: 'button1', text: 'Press Me!',x:200,y:100 });
+        
         var gridConfig = { rows: 5, cols: 5, scene: this };
         var alignGrid = new AlignGrid(gridConfig);
         alignGrid.showNumbers();
 
         emitter = new Phaser.Events.EventEmitter();
         controller = new Controller();
-
+        
         this.sb = new ScoreBox({ scene: this });
         this.sb.x = game.config.width / 2;
         this.sb.y = 50;
