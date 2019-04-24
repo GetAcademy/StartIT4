dea77e833b77fa03f48d845ac571ba7132d3ee36
TommyWi <47241834+TommyWi@users.noreply.github.com>
2019-04-24 08:52:33 +0200

    pong frenzy finished

--- student/tommy/Phaser Games/Pong Frenzy/js/scenes/SceneOver.js
+++ student/tommy/Phaser Games/Pong Frenzy/js/scenes/SceneOver.js
@@ -4,13 +4,13 @@ class SceneOver extends Phaser.Scene {
     }
     preload() {
         this.load.image("button1", "images/ui/buttons/2/1.png");
-        this.load.image("title", "images/ui/title.png");
+        this.load.image("title", "images/title.png");
     }
     create() {
        
 
         this.alignGrid = new AlignGrid({ rows: 11, cols: 11, scene: this });
-        this.alignGrid.showNumbers();
+        //this.alignGrid.showNumbers();
 
 
         var title = this.add.image(0, 0, 'title');
