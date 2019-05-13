1db8f76eefe9770d6fd85ca038ba4921a067825b
spels47 <34216925+spels47@users.noreply.github.com>
2019-05-13 17:32:57 +0200

    x

--- student/patrick/vuetest/dasgay/src/components/Superstar.vue
+++ student/patrick/vuetest/dasgay/src/components/Superstar.vue
@@ -3,12 +3,14 @@
 <div>
 <h1>Superstar Superduper CookieClicker Best Fucking Cookie Clicker in all of мосцоw</h1>
 <clickerbutton @new-count="HandleNewCount"></clickerbutton>
-<h1 :style="{textAlign: 'center'}">{{count}}</h1>
+<h1 :style="{textAlign: 'center', color: CurrentColor, textShadow: '-1px -1px -1px black'}">{{count}}</h1>
+<Navigation Location="Home"></Navigation>
 </div>
 </template>
 
 <script>
 import clickerbutton from '@/components/subcomponents/clickerbutton'
+import Navigation from '@/components/subcomponents/Navigation'
 
 export default
 {
@@ -26,16 +28,24 @@ export default
         'pink',
         'yellow'
       ],
+      CurrentColor: 'white',
+      Place: 'HelloWorld',
     }
   },
   components:
   {
   clickerbutton,
+  Navigation,
   },
   methods: {
+    RandomNumber( min, max )
+            {
+                return Math.floor( Math.random() * ( max - min ) + min )
+            },
     HandleNewCount(RecievedCount)
     {
       this.count=RecievedCount;
+      this.CurrentColor = this.RandomColor[this.RandomNumber(0,this.RandomColor.length)];
     },
   },
 }
