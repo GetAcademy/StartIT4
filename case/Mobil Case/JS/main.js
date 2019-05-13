64bb0c5bd2bdb3cbb89300ea08deec852bb21f79
EMILO <emilberolsen@gmail.com>
2019-05-13 14:09:08 +0200

    Update main.js

--- case/Mobil Case/JS/main.js
+++ case/Mobil Case/JS/main.js
@@ -12,3 +12,17 @@
       <div><button class="button" onclick="kalender()">Kalender</button></div>
     `;
 }
+
+ var updateinputEmpty = setInterval(inputEmpty, 1000);
+        function inputEmpty() {
+            var input1 = document.getElementById('input').value;
+            var input2 = document.getElementById('input').value;
+            var input3 = document.getElementById('input').value;
+            if (input1 == "" || input2 == "" || input3 == "") {
+                document.getElementById('btn1').style.cssText = "visibility: hidden;";
+                document.getElementById('btn2').style.cssText = "visibility: hidden;";
+                document.getElementById('btn3').style.cssText = "visibility: hidden;";
+                document.getElementById('content').innerHTML += `<div>Ingen mål har blitt lagt til.</div>`;
+                return false;
+            }
+        }
