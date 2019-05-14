f3421ebafca9cb0e043e7f4427ff0eb707658532
JRAllum <allum434@gmail.com>
2019-05-14 14:25:45 +0200

    Moodle Oppgaver

---
+++ student/janrobert/Oppgaver/TrePåRadRev/Cell.js
@@ -0,0 +1,32 @@
+class Cell {
+    contructor(index) {
+        this._content = null;
+        this._index = index;
+    }
+
+    setX() {
+        this._content = 'X';
+    }
+    setO() {
+        this._content = 'O';
+    }
+    isBlank() {
+        return this._content === null;
+    }
+    isX() {
+        return this._content === 'X';
+    }
+    isFirstInRow() {
+        return this._index % 3 == 0;
+    }
+    isBlank() {
+        return this._content = null;
+    }
+    index() {
+        return this._index;
+    }
+    content() {
+        if(this.isBlank()) return '';
+        return this._content;
+    }
+}
