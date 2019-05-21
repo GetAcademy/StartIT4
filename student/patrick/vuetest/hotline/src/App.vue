a9c04ac0ab113b49299d1cb0f86801a316554eb8
spels47 <34216925+spels47@users.noreply.github.com>
2019-05-21 18:17:06 +0200

    x

--- student/patrick/vuetest/hotline/src/App.vue
+++ student/patrick/vuetest/hotline/src/App.vue
@@ -2,24 +2,28 @@
   <div id="app">
     <div class="GridContainer">
       <div class="Menu">
-        <button onclick="SettingsPage()">Settings Page</button>
-        <div id="Hotline">
+        <button v-if="this.$router.currentRoute.name != 'Login' && this.$router.currentRoute.name != 'NewAccount'" class="MenuButton" @click="this.$router.push('/Swipe')">Swipe Page</button>
+        <button v-if="this.$router.currentRoute.name != 'Login' && this.$router.currentRoute.name != 'NewAccount'" class="MenuButton" @click="$router.push('/Settings')">Settings Page</button>
+        <div class="Hotline">
           Hotline
         </div>
-        <button onclick="MessagePage()">Message Page</button>
+        <button v-if="this.$router.currentRoute.name != 'Login' && this.$router.currentRoute.name != 'NewAccount'" class="MenuButton" @click="$router.push('/Messages')">Message Page</button>
+        <button v-if="this.$router.currentRoute.name != 'Login' && this.$router.currentRoute.name != 'NewAccount'" class="MenuButton" @click="this.$router.push('/Dates')">Blind Dates</button>
       </div>
       <div class="Middle">
         <router-view/>
       </div>
       <div class="Bottom">
-        <input v-if="this.$router.currentRoute.name == 'Message'" @keydown.enter="AddChat" class="ChatBox" type="text" v-bind="ChatBox"/>
-        <button v-if="this.$router.currentRoute.name == 'Message'" class="Send" @click="AddChat">Send</button>
+        <input v-if="this.$router.currentRoute.name == 'Messages'" @keydown.enter="AddChat" class="ChatBox" type="text" v-model="ChatBox"/>
+        <button v-if="this.$router.currentRoute.name == 'Messages'" class="Send" @click="AddChat">Send</button>
       </div>
     </div>
   </div>
 </template>
 
 <style>
+/* the chat inputs and buttons need a way to get the variables css and html and all to work with router view so that it can function on the messages page */
+
 #app
 {
     margin: 0px;
@@ -31,6 +35,7 @@ button
     text-shadow: 1px 0 0 #AD00FF, -1px 0 0 #AD00FF, 0 1px 0 #AD00FF, 0 -1px 0 #AD00FF, 1px 1px #AD00FF, -1px -1px 0 #AD00FF, 1px -1px 0 #AD00FF, -1px 1px 0 #AD00FF;
     background: rgba(236, 68, 251, 0.75);
     border: 1px solid #AD00FF;
+    border-radius: 20px;
 }
 
 *,* :before, *:after{
@@ -38,13 +43,7 @@ button
   margin:0;
 }
 
-#Hotline{
-    text-align: center;
-    color: #000000;
-    text-shadow: 1px 0 0 #FF0000, -1px 0 0 #FF0000, 0 1px 0 #FF0000, 0 -1px 0 #FF0000, 1px 1px #FF0000, -1px -1px 0 #FF0000, 1px -1px 0 #FF0000, -1px 1px 0 #FF0000;
-    margin-top:2.5vh;
-    font-size:larger;
-}
+
 
 .GridContainer {
     height: 100vh;
@@ -60,7 +59,31 @@ button
     grid-area: Menu;
     background: rgba(236, 68, 251, 0.75);
     border: 1px solid #AD00FF;
+    display:flex;
+    flex-direction:row;
+    justify-content:center;
+    align-items: center;
+
+
 }
+
+.Hotline
+{
+    /* text-align: center; */
+    color: #000000;
+    text-shadow: 1px 0 0 #FF0000, -1px 0 0 #FF0000, 0 1px 0 #FF0000, 0 -1px 0 #FF0000, 1px 1px #FF0000, -1px -1px 0 #FF0000, 1px -1px 0 #FF0000, -1px 1px 0 #FF0000;
+    /* margin-top:2.5vh; */
+    margin-left: 1vw;
+    margin-right: 1vw;
+    font-size:larger;
+}
+
+.MenuButton
+{
+    margin-left: 2vw;
+    margin-right: 2vw;
+}
+
 .Middle
 {
     grid-area: Middle;
