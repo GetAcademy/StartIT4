48d68fe93d71369f2471d47fc163351b464c0e30
Henrik Bjørhall <47069120+Orphanum@users.noreply.github.com>
2019-06-05 15:02:16 +0200

    Cleaning

---
+++ student/henrik/CS/PasswordGenerator/PasswordGenerator/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace PasswordGenerator
+{
+    class Program
+    {
+        static void Info()
+        {
+                Console.WriteLine("Password Generator");
+                Console.WriteLine("  Options: ");
+                Console.WriteLine("     - l = lower case letter");
+                Console.WriteLine("     - L = upper case letter");
+                Console.WriteLine("     - d = digit");
+                Console.WriteLine("     - s = special character");
+                Console.WriteLine("Example: PasswordGenerator 14 Llssdd");
+                Console.WriteLine("     Min. 1 lower case");
+                Console.WriteLine("     Min. 2 upper case");
+                Console.WriteLine("     Min. 2 special characters");
+                Console.WriteLine("     Min. 2 digits");    
+            
+        }
+
+        static void Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                Info();
+            }
+            else if (args.Length == 2)
+            {
+                Console.WriteLine("Test");
+            }
+        }
+    }
+}
