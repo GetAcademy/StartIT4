6cdca7229426e2b77411e047158dd35fd5318bfe
Henrik Bjørhall <47069120+Orphanum@users.noreply.github.com>
2019-08-06 10:38:15 +0200

    Ay

---
+++ student/henrik/CS/Chess/SimpleChess-master/SimpleChess/Piece.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace SimpleChess
+{
+    abstract class Piece
+    {
+        public string Symbol { get; }
+        private readonly string _type;
+
+        protected Piece(string type, string symbol)
+        {
+            _type = type;
+            Symbol = symbol;
+        }
+
+        public Rook();
+        public Bishop();
+        public abstract bool Move(string fromPosition, string toPosition);
+    }
+}
